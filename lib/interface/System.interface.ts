0e34debaaffe643883387bd2610a39ffb22dedee
LucasScotta <lucasmanuelscotta@gmail.com>
2023-05-24 11:56:09 -0300

    old system files

--- lib/interface/System.interface.ts
+++
@@ -1,6 +0,0 @@
-import { TableInterface } from "./Table.interface"
-
-export interface SystemInterface {
-    tables: TableInterface[]
-    init(arg0: number): void
-}
--- lib/system/interface/Product.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface ProductInterface {
-    id: number
-    name: string
-    price: number
-    setName(arg0: string): void
-    setPrice(arg0: number): void
-    getName(): string
-    getPrice(): number
-}
