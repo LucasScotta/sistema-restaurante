4324355683bf80ea0122a288fab84b8698e2950f
LucasScotta <lucasmanuelscotta@gmail.com>
2023-05-27 11:27:13 -0300

    getter in DB added

--- lib/routes/users.ts
+++ lib/routes/users.ts
@@ -7,3 +7,8 @@ export function setNewUser(name: string, password: string) {
     users.push({ id, name, password })
     id += 1
 }
+
+export function getUser(name: string, password: string) {
+    const user = users.find(user => user.name === name && user.password === password)
+    return user
+}
