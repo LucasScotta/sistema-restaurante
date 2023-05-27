3413dda2f72a6540344bfc12b5acb755bf020883
LucasScotta <lucasmanuelscotta@gmail.com>
2023-05-27 11:26:42 -0300

    paths in auth module fixed

--- lib/routes/auth.ts
+++ lib/routes/auth.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { User } from "../model";
+import { getUser, setNewUser, users } from "./users";
 
 const router = Router()
-let id = 1
-const users: User[] = [{ id: 0, name: 'dunfu', password: 'as' }]
+
+router.post('/create', (req, resp) => {
+    const { name, password }: { name: string | undefined, password: string | undefined } = req.body
+    if (!!name && !!password) {
+        const user: User | undefined = users.find(user => user.name === name && user.password === password)
+        if (!!user) {
+            return resp.status(409).json({ message: 'User already exists' })
+        }
+
+        return resp.json(setNewUser(name, password))
+    }
+    return resp.status(400).json({ message: 'Forbidden credentials' })
+})
 
 router.get('/', (req, resp) => {
     resp.json(users)
@@ -12,11 +24,13 @@ router.get('/', (req, resp) => {
 router.post('/', (req, resp) => {
     const { name, password }: { name: string | undefined, password: string | undefined } = req.body
     if (!!name && !!password) {
-        const user: User = { id, name, password }
-        id += 1
-        return resp.send(user)
+        const user: User | undefined = getUser(name, password)
+        if (!!user) {
+            return resp.json(user)
+        }
+        return resp.status(401).json({ message: 'Invalid credentials' })
     }
-    return resp.sendStatus(401)
+    return resp.status(400).json({ message: 'Forbidden credentials' })
 })
 
 export { router as auth }
