b8f5d1ba788fee69494b80fb97fb1ceb22d65af4
LucasScotta <lucasmanuelscotta@gmail.com>
2023-05-28 00:35:07 -0300

    enviroment variables updated

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,6 @@
 import { config } from 'dotenv'
 
 config()
-export const PORT = process.env.PORT
+
+export const PORT = process.env.PORT || 8080
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'TEST_KEY'
