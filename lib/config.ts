7e956122a58f32f8e56f16551d47572bbfed3205
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-02 12:44:10 -0300

    database env variables added to config file

--- lib/config.ts
+++ lib/config.ts
@@ -4,3 +4,6 @@ config()
 
 export const PORT = process.env.PORT || 8080
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'TEST_KEY'
+export const DB_NAME = process.env.DB_NAME || 'bussiness'
+export const DB_USER_NAME = process.env.DB_USER_NAME || 'admin'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'admin'
