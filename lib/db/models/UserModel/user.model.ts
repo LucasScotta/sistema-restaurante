d42d690a1806b678bbfa62972834cb0fa3372b04
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-04 14:27:29 -0300

    user model DB created

---
+++ lib/db/models/UserModel/user.model.ts
@@ -0,0 +1,36 @@
+import { DataTypes, Sequelize } from "sequelize";
+import pg from 'pg';
+
+const sequelize = new Sequelize('bussiness', 'admin', 'admin', {
+    host: 'localhost',
+    port: 7777,
+    dialect: 'postgres',
+    dialectModule: pg,
+    logging: false
+})
+
+export const User = sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        unique: true,
+    },
+    password: DataTypes.STRING,
+    createdAt: {
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.fn("NOW")
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        field: 'updated_at',
+        allowNull: true
+    },
+})
+
+
+
