a1feb952d1cc1ea849d46f5e0a1e7fceadf08d4e
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-05 00:39:35 -0300

    roles attribute added to user response on query DB

--- lib/db/models/UserSchema/user.model.ts
+++ lib/db/models/UserSchema/user.model.ts
@@ -1,4 +1,5 @@
 import { ModelDefined, Optional } from "sequelize"
+import { RolAttributes } from "../RolSchema"
 
 export interface UserAttributes {
     id: number
@@ -6,6 +7,7 @@ export interface UserAttributes {
     password: string
     createdAt: string
     updatedAt: string
+    roles?: RolAttributes | null
 }
 export type createUserAttributes = Optional<UserAttributes, 'id' | 'createdAt' | 'updatedAt'>
 export type UserSchemaType = ModelDefined<UserAttributes, createUserAttributes>
