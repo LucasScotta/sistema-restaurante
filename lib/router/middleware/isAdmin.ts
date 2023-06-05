dc60e1452b1196f30e09a9b85b7c838f0c0558d7
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-05 11:26:25 -0300

    is admin middleware created

---
+++ lib/router/middleware/isAdmin.ts
@@ -0,0 +1,28 @@
+import { Handler } from "express";
+import { JwtPayload, verify } from "jsonwebtoken";
+import { JWT_SECRET_KEY } from "../../config";
+export const isAdmin: Handler = async (req, resp, next) => {
+
+    try {
+        const { authorization } = req.headers
+        if (!authorization) return resp.status(400).json({
+            message: 'Provide your access token'
+        })
+
+        const token = authorization.split(' ').pop()
+        if (!token) {
+            return resp.status(400).json({ status: 'failed', message: 'Invalid token or expired' })
+        }
+        const { user } = verify(token, JWT_SECRET_KEY) as JwtPayload
+        const { rol } = user
+        if (!rol || rol !== 'admin') return resp.status(401).json({
+            status: 'failed', message: 'You have not admin credentials'
+        })
+        console.log(user)
+        return next()
+    }
+    catch (e) {
+        console.log(e)
+        return resp.status(400).json({ status: 'failed', message: 'Invalid token or expired' })
+    }
+}
