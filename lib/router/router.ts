a0e10e231469714ccb830c52ceff79889acf2145
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-05 11:26:43 -0300

    is admin middleware implemented on admin route

--- lib/router/router.ts
+++ lib/router/router.ts
@@ -1,7 +1,8 @@
 import { Router } from "express"
 import { auth, admin } from "../routes"
+import { isAdmin } from "./middleware"
 export const router = Router()
 
 router.use('/auth', auth)
-router.use('/admin', admin)
+router.use('/admin', isAdmin, admin)
 router.get('/', (_req, resp) => resp.send('home'))
