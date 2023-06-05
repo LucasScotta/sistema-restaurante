c03d6954c79d92ed00d5815f835bb504f3128bea
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-05 00:38:40 -0300

    create user service added

--- lib/routes/services/createUser/createUser.ts
+++ lib/routes/services/createUser/createUser.ts
@@ -1,6 +1,4 @@
 import { Request, Response } from "express";
-import { User } from "../../../model";
-import { setNewUser, users } from "../../users";
 import { UserCreationDTO } from "../../../model/UserCreationDTO";
 import { sequelize } from "../../../db";
 
@@ -9,5 +7,8 @@ export const createUser = async (req: Request, resp: Response) => {
     if (!username || !password) return resp.status(400).json({ message: 'Forbidden credentials' })
 
     const user = await sequelize.createUser({ username, password, rol })
-    return resp.json(user)
+    if (user instanceof Error) {
+        return resp.status(403).json({ messasge: user.message })
+    }
+    return resp.status(200).json(user)
 }
