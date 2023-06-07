86884b020e57ddb9f66b4ecf4e3a1ef7c0bb656d
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-06 21:09:56 -0300

    createUser implemented on admin router

--- lib/routes/admin.ts
+++ lib/routes/admin.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { deleteUser, getUsers, updateUser } from "./services";
+import { createUser, deleteUser, getUsers, updateUser } from "./services";
 
 const router = Router()
 
 router.get('/users', getUsers)
+router.post('/users', createUser)
 router.put('/users', updateUser)
 router.delete('/users', deleteUser)
 
