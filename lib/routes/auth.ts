f4e8061843d4f7eba93cad5aaabcb05d8f78563d
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-06 21:24:00 -0300

    createUser and deleteUser removed from auth router

--- lib/routes/auth.ts
+++ lib/routes/auth.ts
@@ -3,16 +3,9 @@ import { login, createUser, deleteUser } from "./services";
 
 const router = Router()
 
-router.post('/create', createUser)
-
-router.get('/', (req, resp) => {
-    resp.json({ users: [] })
-})
-
 //Esto es para el login
 // const token = sign({ username }, JWT_SECRET_KEY, { expiresIn: '2h' })
 router.post('/', login)
 
-router.delete('/', deleteUser)
 
 export { router as auth }
