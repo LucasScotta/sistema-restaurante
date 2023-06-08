8567502e67fbbd840b851ee8cbf10ddfde0af769
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-08 09:31:12 -0300

    isAdmin middleware implemented in admin route

--- lib/router/router.ts
+++ lib/router/router.ts
@@ -1,8 +1,8 @@
 import { Router } from "express"
 import { auth, admin } from "../routes"
-import { isAdmin } from "./middleware"
+import { isAdmin, jwtMiddleware } from "./middleware"
 export const router = Router()
 
 router.use('/auth', auth)
-router.use('/admin', isAdmin, admin)
+router.use('/admin', jwtMiddleware, isAdmin, admin)
 router.use('/*', (_req, resp) => resp.status(404).send('NOT FOUND'))
